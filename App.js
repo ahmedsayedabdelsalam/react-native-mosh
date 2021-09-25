78852f821e7c8ad401a5106143801a1b6fb3175d
Ahmed Sayed <e.eng.ahmedsayed@gmail.com>
2021-09-25 06:24:56 +0200

    Init

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AppText from "./app/components/AppText";
+import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ListingDetailsScreen />
+    // <View
+    //   style={{
+    //     backgroundColor: "#f8f4f4",
+    //     padding: 20,
+    //     paddingTop: 100,
+    //   }}
+    // >
+    //   <Card
+    //     title="Red jacket for sale"
+    //     subTitle="$100"
+    //     image={require("./app/assets/jacket.jpg")}
+    //   />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app/components/AppText.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet, Platform } from "react-native";
+
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "tomato",
+    ...Platform.select({
+      ios: {
+        fontSize: 20,
+        fontFamily: "Avenir",
+      },
+      android: {
+        fontSize: 18,
+        fontFamily: "Roboto",
+      },
+    }),
+  },
+});
+
+export default AppText;
